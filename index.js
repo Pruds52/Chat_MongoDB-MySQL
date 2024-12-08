b6e6ba5160e67a7b7f1777426d32a8a6b680939f
Pruds52 <prudenciopvp@gmail.com>
2024-12-08 16:10:17 -0300

    Implementando o save das mensagens ao banco de dados de maneira dinâmica

--- index.js
+++ index.js
@@ -14,27 +14,54 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', ejs.renderFile);
 
 app.use('/', (req, res) => {
-res.render('index.html');
+    res.render('index.html');
 });
+
+function connectDB() {
+
+    let dbUrl = '';
+
+    mongoose.connect(dbUrl);
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+    mongoose.connection.once('open', function callback(){
+        console.log("Atlas mongoDB conectado!");
+    });
+}
+
+connectDB();
+
+let Message = mongoose.model('Message',{ author : String, data_hora : String, message : String});
 
 let messages = [];
 
-io.on('connection', socket => {
-    console.log('Novo usuário conectado! ID: ' + socket.id)
-    console.log('Novo usuário conectado! ID: ' + socket.id);
-
-    socket.emit('previousMessage', messages);
-    
-    socket.on('sendMessage', data => {
-        messages.push(data);
-
-        socket.broadcast.emit('receivedMessage', data);
-
-
-    });
-
+Message.find({})
+    .then(docs=>{
+        console.log('DOCS: ' + docs);
+        messages = docs;
+        console.log('MESSAGES: ' + messages);
+    }).catch(err=>{
+        console.log(err);
 });
 
-server.listen(3000, () => {
-console.log('http://localhost:3000')
+io.on('connection', socket=>{
+
+    console.log(`Novo usuário conectado ${socket.id}`);
+
+    socket.emit('previousMessage', messages);
+
+    socket.on('sendMessage', data => {
+
+    let message = new Message(data);
+    message.save()
+        .then(
+            socket.broadcast.emit('receivedMessage', data)
+        )
+        .catch(err=>{
+            console.log('ERRO: ' + err);
+        });
+    });
 })
+
+server.listen(3000, () => {
+    console.log('http://localhost:3000')
+});
